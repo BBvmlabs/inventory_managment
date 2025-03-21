5980622ebb154a11a6809de45471c560418b7ac3
Badhri Narayanan Sankar <104076909+BBvmlabs@users.noreply.github.com>
2025-03-21 20:41:32 +0530

    deleted unwanted files

--- controller/categroy_get.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from models.db import get_db_connection
-
-def get_all_categories():
-    """Retrieves all categories from the database"""
-    conn = get_db_connection()
-    categories = conn.execute('SELECT * FROM category').fetchall()
-    conn.close()
-    return render_template('category.html', categories=categories)
--- controller/product_get.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from models.db import get_db_connection
-
-def get_all_products():
-    """Retrieves all products from the database"""
-    conn = get_db_connection()
-    products = conn.execute('SELECT * FROM product').fetchall()
-    conn.close()
-    return render_template('product.html', products=products)
